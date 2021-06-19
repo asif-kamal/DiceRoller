579ec870726eb27ff29984280218494397cbd3a7
asif-kamal <asif.kamal1489@gmail.com>
2021-06-19 11:29:33 -0400

    Built the rollDice function and updtaed screen with dice roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,15 +6,29 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/** This activity allows users to roll a dice and view the result on the screen */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//Set on click listener to trigger the rollDice() function
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /** Roll the dice and get the updated number on the screen */
+    private fun rollDice() {
+        //Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        //Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
