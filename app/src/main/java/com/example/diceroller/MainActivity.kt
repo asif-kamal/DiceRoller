9c9e0eefd6a15dd975f39a5cc7d356769fa96ada
asif-kamal <asif.kamal1489@gmail.com>
2021-06-20 12:36:08 -0400

    Added drawable resource for second dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -22,10 +22,25 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         //Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
+        val dice2 = Dice(6)
         val diceRoll = dice.roll()
+        //Created second instance of 6 sided dice.
+        val dice2Roll = dice2.roll()
         //Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
-        // Determine which drawable resource ID to use based on the dice roll
+        //Find the Imageview for the second dice.
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        /** Determine which drawable resource ID to use based on the dice roll
+        Built a second drawable resource for second dice imageview. */
+        val drawableResource2 = when (dice2Roll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -34,10 +49,14 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+
         // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+
+        diceImage2.setImageResource(drawableResource2)
         //Update content description.
         diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = dice2Roll.toString()
        }
     }
 
